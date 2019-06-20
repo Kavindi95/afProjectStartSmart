50ebe01c98a8e7e1309493788350d58d8c6aa9cd
AlphaSpirits <alpha.4spirits@gmail.com>
2019-06-20 16:53:57 +0530

    update with instructor functions

---
+++ smartclient/src/components/Instructor/addAssignment/addAssignment.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class AddAssignment extends Component{
+
+    render(){
+        return(
+            <div>
+            <h1>Generate Assignment</h1>
+            </div>
+            <div className="container">
+                <form className="form">
+                    <div className="form-group">
+                    <label>Course Name</label>
+                    <input
+                        type="text"
+                        name="coursename"
+                        className="form-control"
+                        />
+                    </div>
+                    <div className="form-group>
+                    <label>Subject Name</label>
+                    <input
+                        type="text"
+                        name="subjectname"
+                        className="form-control"
+                    />
+                    </div>
+                    <div className="form-group">
+                    <label>Assignment Number</label>
+                    <input
+                        type="text"
+                        name="assignmentnumber"
+                        className="form-control"
+                    />
+                    </div>
+                    <div className="form-group">
+                    <label>Due Date</label>
+                    <input
+                        type="text"
+                        name="duedate"
+                        className="form-control"
+                        />
+                    </div>
+                    <div className="form-group">
+                    <input
+                        type="submit"
+                        name="submitAssignment"
+                        value="Submit"
+                    />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+export default AddAssignment;
---
+++ smartclient/src/components/Instructor/updateExam/updateExam.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+class UpdateExam extends Component{
+
+    render(){
+        return(
+            <div>
+            <h1>Edit Exam</h1>
+        </div>
+        <div className="container">
+            <form className="form">
+            <div className="form-group">
+            <label>Course Name</label>
+        <input
+        type="text"
+        name="coursename"
+        className="form-control"
+            />
+            </div>
+            <div className="form-group>
+            <label>Subject Name</label>
+        <input
+        type="text"
+        name="subjectname"
+        className="form-control"
+            />
+            </div>
+            <div className="form-group">
+            <label>Exam Type</label>
+        <input
+        type="text"
+        name="examType"
+        className="form-control"
+            />
+            </div>
+            <div className="form-group">
+            <label>Due Date</label>
+        <input
+        type="text"
+        name="dueDate"
+        className="form-control"
+            />
+            </div>
+            <div className="form-group">
+            <label>Description</label>
+            <input
+        type="text"
+        name="description"
+        className="form-control"
+            />
+            </div>
+            <div className="form-group">
+            <input
+        type="submit"
+        name="submitAssignment"
+        value="Submit"
+            />
+            </div>
+            </form>
+            </div>
+    )
+    }
+}
+export default UpdateExam;
