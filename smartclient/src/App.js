e41b62fcd3b367f7def8a01ae34a87b8f39e2479
AlphaSpirits <alpha.4spirits@gmail.com>
2019-06-20 15:38:32 +0530

    update of frontent with smartclient

---
+++ smartclient/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from "reactstrap";
+
+import Page from "./components/Dashboard/Page";
+
+function App() {
+  return (
+      <div>
+      <Container>
+      <Page />
+      <br />
+
+      <br />
+      </Container>
+      </div>
+  );
+}
+
+export default App;
