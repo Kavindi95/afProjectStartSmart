17afaf6689fc9fa3e3ef926cf3c326fd44cc0907
AlphaSpirits <alpha.4spirits@gmail.com>
2019-06-21 13:19:46 +0530

    mongo atlas server changes

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyPorser = require("body-parser");
+const mongoose = require("mongoose");
+
+const instructorAddAssignment = require("./routes/instructor/addassignments");
+
+mongoose.connect(
+    "mongodb+srv://tharuka:tharuka12345@afprojectstartsmart-ycawy.mongodb.net/test?retryWrites=true&w=majority",
+    {
+     
+      useNewUrlParser: true
+    }
+  );
+
+  app.use(morgan("dev"));
+  app.use(bodyPorser.urlencoded({ extended: false }));
+  app.use(bodyPorser.json());
+
+  app.use("/addassignment", instructorAddAssignment);
+
+
+  app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+  });
+
+  
+app.get('/',(req,res,next)=>{
+  res.status(200).json({
+    message:"its working"
+  })
+})
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+  });
+  module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,28 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
-app.use(cors());
-
-const PORT = process.env.PORT || 5000;
-
-//connect to Mongoose
-mongoose.connect('mongodb://localhost/startSmart');
-var db = mongoose.connection;
-
-/*
-* Load the application
-* */
-app.get('/', function (req,res) {
-    res.send('welcome start smart');
-});
-
-app.listen(PORT , () => {
-    console.log(`Running on PORT : ${ PORT }`);
-});
-
+const http = require('http');
+const app = require('./app');
+const port = process.env.port || 3000;
+const server = http.createServer(app);
+console.log(port);
+server.listen(port);
