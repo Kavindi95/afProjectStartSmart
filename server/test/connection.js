59f08c18a968583a07e093a658cce22b44688db8
kavindi95 <kavipabasara@gmail.com>
2019-06-22 15:05:21 +0530

    Added Unit Test Case for Saving an Instructor

---
+++ server/test/connection.js
@@ -0,0 +1,20 @@
+//K
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+before(function(done) {
+  this.timeout(5000);
+  mongoose.connect(
+    "mongodb+srv://tharuka:tharuka12345@afprojectstartsmart-ycawy.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true
+    }
+  );
+  mongoose.connection
+    .once("open", function() {
+      console.log("Successfully Connected to the Database");
+      done();
+    })
+    .on("error", function(error) {
+      console.log.log(error);
+    });
+});
