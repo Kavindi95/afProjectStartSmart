b94b919b966af262d2ce0e043262861a3f012a0c
AlphaSpirits <alpha.4spirits@gmail.com>
2019-06-22 16:31:50 +0530

    server changes for file upload

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const bodyPorser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require('cors');
 //file upload
-
+const fileUpload = require('express-fileupload');
 
 const instructorAddAssignment = require("./routes/instructor/addassignments");
 const admin = require("./routes/admin/adminRoutes");
@@ -22,6 +22,8 @@ app.use(morgan("dev"));
 app.use(bodyPorser.urlencoded({ extended: false }));
 app.use(bodyPorser.json());
 app.use(cors());
+//file upload
+app.use(fileUpload());
 
 app.use("/addassignment", instructorAddAssignment);
 app.use("/admin", admin);
@@ -45,5 +47,22 @@ app.use((error, req, res, next) => {
       message: error.message
     }
   });
+});
+
+//file upload endpoint
+app.post('/upload', (req,res) => {
+  if(req.files == null){
+    return res.status(400).json({msg: 'No File Uploaded'});
+  }
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/../smartclient/public/uploads/${file.name}`, err => {
+    if (err){
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}`});
+  })
 });
 module.exports = app;
