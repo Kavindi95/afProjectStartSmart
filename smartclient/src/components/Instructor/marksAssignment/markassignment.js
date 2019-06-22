82e84c224507ade1fa957b1260b63f6e9040a79c
kavindi95 <kavipabasara@gmail.com>
2019-06-22 16:14:21 +0530

    new commit for instructor dashboard

--- smartclient/src/components/Instructor/marksAssignment/markassignment.js
+++ smartclient/src/components/Instructor/marksAssignment/markassignment.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
-class UpdateAssignment extends Component{
+class markAssignment extends Component{
 
-    updateassignmentss(e){
+    entermarks(e){
 
         e.preventDefault();
         console.log(this.refs.cname.value);
         console.log(this.refs.subname.value);
-        console.log(this.refs.year.value);
-        console.log(this.refs.sem.value);
-        console.log(this.refs.date.value);
+        console.log(this.refs.std.value);
+        console.log(this.refs.assnum.value);
+        console.log(this.refs.marks.value);
 
     }
 
@@ -17,7 +17,7 @@ class UpdateAssignment extends Component{
         return(
             <div>
             <div>
-            <h1>Edit Assignment</h1>
+            <h1>Assign Marks</h1>
             </div>
             <div className="container">
             <form className="form">
@@ -40,38 +40,41 @@ class UpdateAssignment extends Component{
             />
             </div>
             <div className="form-group">
-            <label>Year</label>
+            <label>Student ID number</label>
         <input
         type="text"
         name="subjectname"
         className="form-control"
-        ref="year"
+        ref="std"
             />
             </div>
+            
             <div className="form-group">
-            <label>Semester</label>
+            <label>Assignment Number</label>
         <input
         type="text"
         name="subjectname"
         className="form-control"
-        ref="sem"
+        ref="assnum"
             />
             </div>
             <div className="form-group">
-              <label>Due Date</label>
-              <input type="date" 
-              name="duedate" 
-              className="form-control" 
-              ref="date" />
+            <label>Marks</label>
+        <input
+        type="text"
+        name="subjectname"
+        className="form-control"
+        ref="marks"
+            />
             </div>
 
             <div className="form-group">
             <button
                 type="button"
                 className="btn btn-warning btn-block"
-                onClick={this.updateassignmentss.bind(this)}
+                onClick={this.entermarks.bind(this)}
               >
-                Update Exam
+                Enter Marks
               </button>
             </div>
             </form>
@@ -80,4 +83,4 @@ class UpdateAssignment extends Component{
     )
     }
 }
-export default UpdateAssignment;
+export default markAssignment;
